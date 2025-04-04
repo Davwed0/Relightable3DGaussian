filtering/masking fixed

scene/dataset_readers.py
@@ -111,7 +111,12 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder, debug=False
         mask_path = os.path.join(os.path.dirname(images_folder), "masks", os.path.basename(extr.name))
         mask = 1.0 - load_mask_bool(mask_path) / 255
         image = image * mask[..., None]
-        
+
+        if image.mean() < 1e-3:
+            print(f"Skipping black image: {image_path}")
+            continue
+
+
         cam_info = CameraInfo(uid=uid, R=R, T=T, FovX=Fovx, FovY=FovY, fx=focal_length_x, fy=focal_length_y, cx=ppx,
                               cy=ppy, image=image, image_path=image_path, image_name=image_name, width=width, height=height,
                               image_mask=mask)
@@ -120,6 +125,22 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder, debug=False
         if debug and idx >= 5:
             break
     sys.stdout.write('\n')
+    output_file = os.path.join(os.path.dirname(images_folder), "camera_info.json")
+    print(f"Writing camera information to {output_file}")
+
+    serializable_cam_infos = []
+    for cam in cam_infos:
+        cam_dict = {
+            "image_path": cam.image_path,
+            "image_name": cam.image_name,
+            "image": float(np.mean(cam.image)),
+            "mask": float(np.mean(cam.image_mask)),
+        }
+        serializable_cam_infos.append(cam_dict)
+
+    with open(output_file, "w") as f:
+        json.dump(serializable_cam_infos, f, indent=2)
+
     return cam_infos
 
 

train.py
@@ -52,15 +52,16 @@ def training(dataset: ModelParams, opt: OptimizationParams, pipe: PipelineParams
     src_path = os.path.join(dataset.model_path, "point_cloud",
                                     f"iteration_{start_iter}")
     ply_path = os.path.join(src_path, "point_cloud.ply")
-    mask_path = os.path.join(src_path, "mask.pt")
+    mask_path = os.path.join(args.source_path, "images", "mask.pt")
 
     if start_iter is not None:
         print(f"Loading from iteration {args.start_iter}...")
         temp_model = GaussianModel(dataset.sh_degree, render_type=args.type)
         if os.path.exists(ply_path):
-            if os.path.exists(mask_path):
+            if os.path.exists(mask_path) and not args.only_pbr:
                 mask = torch.load(mask_path)
                 temp_model.load_ply(ply_path, mask, only_pbr=args.only_pbr)
+                print(f"Loading mask from {mask_path}")
             else:
                 temp_model.load_ply(ply_path, only_pbr=args.only_pbr)
             gaussians = GaussianModel.create_from_gaussians([temp_model], dataset)